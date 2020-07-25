0816eb5b62072c7f371472f3eaa848510a774251
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-07-25 10:48:50 +0200

    Update unit.py

--- bbtest/helpers/unit.py
+++ bbtest/helpers/unit.py
@@ -103,7 +103,7 @@ class UnitHelper(object):
           fd.write(result)
 
         result = [item for item in result.split(os.linesep)]
-        result = [item.rsplit('/', 1)[-1].strip() for item in result if "/lib/systemd/system/cnb-rates" in item]
+        result = [item.rsplit('/', 1)[-1].strip() for item in result if "/lib/systemd/system/ledger" in item]
 
         self.units = result
 
