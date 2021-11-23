ac7b346995707db4ca1ff393edf7fe86ddee6252
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-11-23 08:01:13 +0100

    stabilise tests

--- bbtest/steps/orchestration_steps.py
+++ bbtest/steps/orchestration_steps.py
@@ -6,6 +6,7 @@ import os
 from behave import *
 from helpers.shell import execute
 from helpers.eventually import eventually
+from helpers.http import Request
 
 
 @given('package {package} is {operation}')
@@ -54,24 +55,33 @@ def step_impl(context):
 @given('unit "{unit}" is running')
 @then('unit "{unit}" is running')
 def unit_running(context, unit):
-  @eventually(20)
+  @eventually(10)
   def wait_for_unit_state_change():
     (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
     assert code == 'OK', str(result) + ' ' + str(error)
-    assert 'SubState=running' in result, '{} {}'.format(unit, result)
+    assert 'SubState=running' in result, result
 
   wait_for_unit_state_change()
-  time.sleep(1)
+
+  if 'ledger-rest' in unit:
+    request = Request(method='GET', url="https://127.0.0.1/health")
+
+    @eventually(5)
+    def wait_for_healthy():
+      response = request.do()
+      assert response.status == 200, str(response.status)
+
+    wait_for_healthy()
 
 
 @given('unit "{unit}" is not running')
 @then('unit "{unit}" is not running')
 def unit_not_running(context, unit):
-  @eventually(20)
+  @eventually(10)
   def wait_for_unit_state_change():
     (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
     assert code == 'OK', str(result) + ' ' + str(error)
-    assert 'SubState=running' not in result, '{} {}'.format(unit, result)
+    assert 'SubState=dead' in result, result
 
   wait_for_unit_state_change()
 
