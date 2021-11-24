85533cac51037da9513ac81082bb061a2cbc4c45
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-11-24 21:35:27 +0100

    multiarch docker packaging

--- .jenkins/commit.groovy
+++ .jenkins/commit.groovy
@@ -9,7 +9,7 @@ def dockerOptions() {
     options += "--label 'org.opencontainers.image.authors=${env.PROJECT_AUTHOR}' "
     options += "--label 'org.opencontainers.image.title=${env.PROJECT_NAME}' "
     options += "--label 'org.opencontainers.image.description=${env.PROJECT_DESCRIPTION}' "
-    options += "-f packaging/docker/Dockerfile "
+    options += "-f packaging/docker/amd64/Dockerfile "
     options += "."
     return options
 }
@@ -260,7 +260,7 @@ pipeline {
         stage('Package Docker') {
             steps {
                 script {
-                    DOCKER_IMAGE = docker.build("${env.ARTIFACTORY_DOCKER_REGISTRY}/docker-local/openbank/ledger:${env.VERSION}", dockerOptions())
+                    DOCKER_IMAGE = docker.build("${env.ARTIFACTORY_DOCKER_REGISTRY}/docker-local/openbank/ledger:amd64-${env.VERSION}", dockerOptions())
                 }
             }
         }
