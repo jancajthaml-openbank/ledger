186b2cf26fbc105198eb92e4893c660d6c96729e
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-12-20 17:59:08 +0100

    better journal logging in bbtests

--- bbtest/environment.py
+++ bbtest/environment.py
@@ -28,6 +28,8 @@ def after_scenario(context, scenario):
 
 def before_all(context):
   context.log = logger()
+  context.log.info('')
+  context.log.info('  (START)')
   context.unit = UnitHelper(context)
   context.zmq = ZMQHelper(context)
   context.vault = VaultHelper(context)
@@ -39,6 +41,9 @@ def before_all(context):
 
 
 def after_all(context):
+  context.log.info('')
+  context.log.info('  (END)')
+  context.log.info('')
   context.unit.teardown()
   context.zmq.stop()
   context.statsd.stop()
